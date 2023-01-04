88b15790b86a465a827f630666812bc7d79e34a2
richard santos <richardsantospereira4322@gmail.com>
2023-01-04 19:19:38 -0300

    @richardsantox [Organizando arquivos]

---
+++ src/components/Box.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="box has-text-weight-bold">
+        <slot></slot>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: "BoxAlura"
+})
+</script>
+
+<style scoped>
+.box{
+    background: #FAF0CA;
+}
+</style>
